3e0ac2cfc81fff7d758d2073a30fc5d6cb20fea6
Adele Lynn <virepri2k@gmail.com>
2019-03-26 13:46:57 -0700

    Added clipboard external functions

--- client/internal-command/Auth.go
+++ client/internal-command/Auth.go
@@ -2,27 +2,38 @@ package internal_command
 
 import (
 	"fmt"
+	"github.com/virepri/clipman-desktop/cli/commands"
 	"github.com/virepri/clipman-desktop/config"
 )
 
 func AuthUser(args []string) {
 	buffer := []byte{4, 10}
-	buffer = append(buffer, []byte(args[0])...)
+	buffer = append(buffer, []byte(config.UserHash)...)
 	buffer = append(buffer, 0)
 
-	if _, err := config.Connection.Write(buffer); err != nil {
-		fmt.Println(err)
+	if config.Connection != nil {
+		if _, err := config.Connection.Write(buffer); err != nil {
+			fmt.Println(err)
+		}
+
+		//TODO: handle success/failure conditions
+	} else if args[0] == "cli_request" {
+		commands.Success <- false
 	}
 }
 
 func AuthAdmin(args []string) {
 	buffer := []byte{0, 10}
-	buffer = append(buffer, []byte(args[0])...)
+	buffer = append(buffer, []byte(config.AdminHash)...)
 	buffer = append(buffer, 0)
 
-	if _, err := config.Connection.Write(buffer); err != nil {
-		fmt.Println(err)
-	}
+	if config.Connection != nil {
+		if _, err := config.Connection.Write(buffer); err != nil {
+			fmt.Println(err)
+		}
 
-	//TODO: handle success/failure conditions.
+		//TODO: handle success/failure conditions.
+	} else if args[0] == "cli_request" {
+		commands.Success <- false
+	}
 }
--- client/internal-command/Clip.go
+++ client/internal-command/Clip.go
@@ -1,9 +1,31 @@
 package internal_command
 
-func PushClip(args []string) {
+import (
+	"github.com/virepri/clipman-desktop/cli/commands"
+	"github.com/virepri/clipman-desktop/config"
+	"github.com/virepri/clipman-desktop/monitor"
+)
 
+func PushClip(args []string) {
+	buffer := []byte{2, 10}
+	buffer = append(buffer, []byte(monitor.ClipboardContent)...)
+	buffer = append(buffer, 0)
+
+	if config.Connection != nil && commands.UserPerms {
+		_, _ = config.Connection.Write(buffer)
+		commands.Success <- true
+	} else {
+		commands.Success <- false
+	}
 }
 
 func RefreshClip(args []string) {
+	buffer := []byte{2, 0}
 
+	if config.Connection != nil && commands.UserPerms {
+		_, _ = config.Connection.Write(buffer)
+		commands.Success <- true
+	} else {
+		commands.Success <- false
+	}
 }
